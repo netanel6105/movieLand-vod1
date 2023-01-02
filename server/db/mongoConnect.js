2b41952fdf52445b525ab25a3c2d111a15173567
netanel <netaneal6105@gmail.com>
2023-01-02 10:33:14 +0200

    ok

--- server/db/mongoConnect.js
+++ server/db/mongoConnect.js
@@ -4,6 +4,6 @@ require('dotenv').config()
 main().catch(err => console.log(err)); //Error Message if not working the connect
 
 async function main() { //connect Data Base from Mongo Atlas by user & password 
-    await mongoose.connect(`mongodb://127.0.0.1:27018/myapp`); //(feb22 == name DataBase)
+    await mongoose.connect(`mongodb+srv://koko9:neta@clustercats.lmrhiow.mongodb.net/yarinDb`); //(feb22 == name DataBase)
     console.log(`Mongo Atlas connect...`) //print--> Mongo is Working
 }
