e22ea43e3cab767cd5202908d4e7927dd6d6720b
netanel <netaneal6105@gmail.com>
2023-01-09 12:23:00 +0200

    ok

--- client/src/components/pages/login.jsx
+++ client/src/components/pages/login.jsx
@@ -7,135 +7,147 @@ import { LOGIN_URL, TOKEN_KEY } from '../../constant/constant'
 import { apiPost } from '../../services/services'
 // p pl pr pt pb >> m mr ml mt mb
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
 
-  const {register, handleSubmit, formState: { errors } } = useForm()
-  const [error, setError] = useState('')
-  const emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
-  console.log(errors)
-const nav = useNavigate()
+    const emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+    const nav = useNavigate()
+    const login = async (_body) => {
 
-  const login = async (_body) => {
+        try {
+            setLoading(true)
+            console.log(_body)
+            const { data } = await apiPost(LOGIN_URL, _body)
+            //   console.log(data)
+        
+            setTimeout(()=>{
+                if (data.token) {
+                    localStorage.setItem(TOKEN_KEY, data.token)
+                    if (data.role == 'admin') {
+                        nav('/admin')
+                    } else {
+                        nav('/todo')
+                    }
+                }
+                setLoading(false)
 
-    try {
-      console.log(_body)
-      const { data } = await apiPost(LOGIN_URL, _body)
-      console.log(data)
-      
-      if(data.token){
-          localStorage.setItem(TOKEN_KEY,data.token)
-        if(data.role == 'admin'){
-            nav('/admin')
+            },1000)
+          
+
+
+        } catch (err) {
+            //   console.log(err.response.data)
+            if (err.response.data.err_msg) {
+                setError(err.response.data.err_msg)
+            }
         }
-        else{
-            nav('/todo')
-        }
-      }
-      
-      
-    } catch (err) {
-      console.log(err.response.data)
-      if (err.response.data.err_msg) {
-          setError(err.response.data.err_msg)
-      }
     }
-  }
 
     const onSub = (data) => {
         console.log(data)
         login(data)
-     
-    
     }
     return (
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Login
-            </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{' '}
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                start your 14-day free trial
-              </a>
-            </p>
-          </div>
-          <form onSubmit={handleSubmit(onSub)} className="mt-8 space-y-6" >
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-         
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="w-full max-w-md space-y-8">
+                <div>
+                    <img
+                        className="mx-auto h-12 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                    />
+                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                        Login
+                    </h2>
+                    <p className="mt-2 text-center text-sm text-gray-600">
+                        Or{' '}
+                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                            start your 14-day free trial
+                        </a>
+                    </p>
+                </div>
+                <form onSubmit={handleSubmit(onSub)} className="mt-8 space-y-6" >
+                    <input type="hidden" name="remember" defaultValue="true" />
+                    <div className="-space-y-px rounded-md shadow-sm">
 
-              <div >
-                <label className="sr-only">
-                  Email address
-                </label>
-                <input
-                  {...register('email', { required:{value:true,message:'email is required'}, pattern:{value:emailReg,message:'Invalid email'} })}
-                  type="email"
 
-                  className="mt-3 relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Email address"
-                />
-                {errors.email && <p className='text-red-600'>{errors.email.message} </p>}
-              </div>
-              <div >
-                <label className="sr-only">
-                  password
-                </label>
-                <input
-                  {...register('password', { required: true, minLength: 3, maxLength: 20 })}
-                  type="password"
+                        <div >
+                            <label className="sr-only">
+                                Email address
+                            </label>
+                            <input
+                                {...register('email', { required: { value: true, message: 'email is required' }, pattern: { value: emailReg, message: 'Invalid email' } })}
+                                type="email"
 
-                  className="mt-3 relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="password.."
-                />
-                {errors.password && <p className='text-red-600'>password is required </p>}
+                                className="mt-3 relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                placeholder="Email address"
+                            />
+                            {errors.email && <p className='text-red-600'>{errors.email.message} </p>}
+                        </div>
+                        <div >
+                            <label className="sr-only">
+                                password
+                            </label>
+                            <input
+                                {...register('password', { required: true, minLength: 3, maxLength: 20 })}
+                                type="password"
 
-              </div>
-          
+                                className="mt-3 relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                placeholder="password.."
+                            />
+                            {errors.password && <p className='text-red-600'>password is required </p>}
+
+                        </div>
+
+                    </div>
+                    {error && <p className='text-red-600'>{error}</p>}
+                    <div className="flex items-center justify-between">
+                        {loading ?
+                            <div className="flex items-center">
+                                <img style={{ position: 'absolute', zIndex: '-1' }} width={'80px'} src='https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=ecf05e47nogttyy9l1kfcz2ckyo2oot6pkp8dhvia62f9euw&rid=giphy.gif&ct=g' />
+
+                            </div>
+                            :
+                            <div className="flex items-center">
+                                <input
+                                    id="remember-me"
+                                    name="remember-me"
+                                    type="checkbox"
+                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                />
+                                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                                    Remember me
+                                </label>
+                            </div>
+                        }
+
+
+
+
+                        <div className="text-sm">
+                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                Forgot your password?
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <button
+                            type="submit"
+                            className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                                <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                            </span>
+                            Login
+
+                        </button>
+                    </div>
+                </form>
             </div>
-            {error && <p className='text-red-600'>{error}</p>}
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                />
-                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                  Remember me
-                </label>
-              </div>
-
-              <div className="text-sm">
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                </span>
-                Login
-              </button>
-            </div>
-          </form>
         </div>
-      </div>
 
     )
-  }
+}
 
-  export default Login
+export default Login
